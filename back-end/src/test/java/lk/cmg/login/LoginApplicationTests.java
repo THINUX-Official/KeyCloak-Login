6bde65ee31f8d329c33c84d211748eb96db5b893
THINUX-Official <thinuxofficial@gmail.com>
2024-04-04 11:10:23 +0530

    Create application.yml file.

---
+++ back-end/src/test/java/lk/cmg/login/LoginApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.cmg.login;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
