6842168377b2f91665e8d173628cc8d689116405
THINUX-Official <thinuxofficial@gmail.com>
2024-04-04 11:28:55 +0530

    Create User entity.

---
+++ back-end/src/main/java/lk/cmg/login/entity/User.java
@@ -0,0 +1,23 @@
+package lk.cmg.login.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author: THINUX
+ * @created: 04-Apr-24 - 11:25 AM
+ */
+
+@Entity
+@NoArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private String email;
+}
